42ab58830f791c1c89c227ccfd411319f57601e0
PHProir <phpro.ir@gmail.com>
2014-10-28 22:14:03 +0330

    fix :: when a tab open or activ , check the page for rss link and then set button enable or disable

--- data/content.js
+++ data/content.js
@@ -1,12 +1,18 @@
 self.port.on("getRssLink", function(){
 	var links = document.getElementsByTagName('link'); 
+	var flag = false;
 	for (var i = 0; i < links.length ; ++i)
 	{
 		if (links[i].getAttribute('type') == 'application/rss+xml')
 		{
 			self.port.emit('setRssLink',links[i].getAttribute('href'));
+			flag = true;
 			break;
 		}
+	}
+	console.log('getRssLink EMITTED');
+	if(flag == false){
+		self.port.emit('disableBtn');
 	}
 });
 
